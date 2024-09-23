ba481c0194fdec7e5e6e40b5bafc9de27d1aa7ec
Aayan Joshi <aayan.joshi@hotmail.com>
2024-09-23 23:26:24 +0545

    first commit w/ nav, hero and features section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
+
+const App = () => {
+  return (
+    <mail classname={`overflow-hidden`}>
+      <Header />
+      <Hero />
+      <Features />
+    </mail>
+  );
+};
+export default App;
