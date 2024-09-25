ffdc0de2e39a11427019f05545045188494108c1
Aayan Joshi <aayan.joshi@hotmail.com>
2024-09-25 11:11:05 +0545

    Implement Download Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
-    <mail classname={`overflow-hidden`}>
+    <mail className={`overflow-hidden`}>
       <Header />
       <Hero />
       <Features />
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
+      {/*<Footer />*/}
     </mail>
   );
 };
